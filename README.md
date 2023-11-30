80467f579d0680ddbd5f4919c9a1def596466e74
diouf <geoffreyfleur@gmail.com>
2023-11-30 19:20:28 +0100

    :ukraine: Création du projet

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # react_test_jest
+
+### getting started
+
+```shell
+npm i
+npm start
+```
---
+++ src/utils/math-functions.js
@@ -0,0 +1,15 @@
+function sum(a = 0, b = 0) {
+    return a + b;
+}
+
+function substract(a = 0, b = 0) {
+    return a - b;
+}
+
+function multiply (a = 0, b = 0) {
+    return  Number.parseFloat((a * b).toFixed(2));
+}
+
+function divide(a = 0, b = 0) {
+    return Number.parseFloat((a / b).toFixed(2));
+}
